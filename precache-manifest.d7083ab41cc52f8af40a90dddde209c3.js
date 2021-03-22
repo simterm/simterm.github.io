d79b9c4e262f5eb86146714bb3710d754321c070
simterm <webmaster@simterm.com>
2021-03-22 18:38:01 -0500

    initial commit

---
+++ precache-manifest.d7083ab41cc52f8af40a90dddde209c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "debe10706bec5452e1f37bf2bc7ff436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2142c2975603555405f9",
+    "url": "/static/css/main.b1840394.chunk.css"
+  },
+  {
+    "revision": "5c58702103333e823fdb",
+    "url": "/static/js/2.80214e38.chunk.js"
+  },
+  {
+    "revision": "54e9c7df8753c83e9e05e9915883890e",
+    "url": "/static/js/2.80214e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2142c2975603555405f9",
+    "url": "/static/js/main.dab96f63.chunk.js"
+  },
+  {
+    "revision": "0306f07bf064eabe99fb",
+    "url": "/static/js/runtime-main.90bdfdd8.js"
+  }
+]);
